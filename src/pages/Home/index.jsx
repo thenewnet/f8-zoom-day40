10e2227e8e0d8339696b2fc308008a716f176e7a
trungbq <trungbq86@gmail.com>
2025-10-02 14:30:55 +0700

    update home page and package to publish. DONE

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,10 @@
+
+import styles from './Home.module.scss';
+
 function Home() {
     return (
 
-        <h1>F8 Zoom day 40 - React Redux</h1>
+        <h1 className={styles.pageTitle}>F8 Zoom day 40 - React Redux</h1>
     )
 }
 
